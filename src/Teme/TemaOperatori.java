f96efc1282d6adca73e2191c10b5f057606b1877
marco <marcoalexandrutincul@gmail.com>
2025-09-06 12:17:15 +0300

    Tema Operatori pentru verificare:)

---
+++ src/Teme/TemaOperatori.java
@@ -0,0 +1,57 @@
+package Teme;
+
+public class TemaOperatori {
+
+    public static void main (String []  args){
+
+        int a = 10;
+        int b = 20;
+
+
+        System.out.println(a + b); // 30
+        System.out.println(b - a); // 10
+        System.out.println(a * b); // 200
+        System.out.println(b / a); // 2
+        System.out.println(b % a); // 0
+
+
+
+        double c = 10.5;
+        double d = 4.5;
+        System.out.println(c + d); // 15,0
+        System.out.println(c * d); // 47,25
+        System.out.println(c / d); // 2.3333333333333335
+
+        int e = +10;
+        int f = -20;
+
+        System.out.println(e); // 10
+        System.out.println(f); // -20
+
+        int num = -25;
+        System.out.println(+num);  // -25
+        System.out.println(-num);  // 25
+
+        boolean isFirstTime = true;
+        boolean isRegistered = false;
+
+        System.out.println(!isFirstTime); // false
+        System.out.println(!isRegistered); // true
+
+        boolean flag = false;
+        System.out.println(!flag); // true
+
+        int m = 5;
+        int j = 5;
+        System.out.println(m++); // 5
+        System.out.println(++j); // 6
+
+
+
+
+
+
+    }
+}
+
+
