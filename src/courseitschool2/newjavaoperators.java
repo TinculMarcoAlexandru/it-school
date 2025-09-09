1ea6adb42c9b2d0498d8f89b0ea5bf4c528cf27a
marco <marcoalexandrutincul@gmail.com>
2025-09-09 19:10:04 +0300

    Tema Operatori pentru verificare:)

--- src/courseitschool2/newjavaoperators.java
+++ src/courseitschool2/newjavaoperators.java
@@ -1,6 +1,6 @@
-package Teme;
+package courseitschool2;
 
-public class TemaOperatoriContinuare {
+public class newjavaoperators {
 
     public static void main(String[] args) {
         int x = 10;
@@ -9,22 +9,14 @@ public class TemaOperatoriContinuare {
         int result2 = ++y * 5;
         System.out.println(result1); // 50
         System.out.println(result2); // 55
-
         int value = 20;
         value *= 3; // 60
-
         System.out.println(value); //60
-
         value %= 50; //
-
         System.out.println(value); // 10
-
-
         int a = 10;
         a += 5; // Compound addition. Equivalent to: a = a + 5;
-
         System.out.println(a); // 15
-
         int e = 10;
         int f = 20;
 
@@ -37,27 +29,20 @@ public class TemaOperatoriContinuare {
 
         double c = 15.5;
         int d = 15;
-
         System.out.println(c > d);  // true
         System.out.println(c <= d); // false ( ia egalul prima data)
-
         short m = 10;
         int n = m + 5; // int n takes the value of m which is 10 and adds 5 to it
-
         long g = 100L;
         int h = (int) n; // int is 15 (charc)=int in interiorul codului
-
         System.out.println("My short value is " + m);//10
         System.out.println("My long value is " + g);//100
         System.out.println("My int value is " + n);//15
-
         byte q = 10;
         int o = q;  // because byte turns into int automaticaly when it is assigned a value
 
         double v = 5.5;
         float p = (float) v;  // cast is (float) and it is 5,5
-
-
         System.out.println("My byte value is " + q);//10
         System.out.println("My int value is " + o);
         System.out.println("My float value is " + p);
@@ -73,18 +58,4 @@ public class TemaOperatoriContinuare {
         if (outerVar > 50) {
             int innerVar = 200;
             System.out.println(innerVar + outerVar); // 300
-        }
-
-
-
-
-
-
-
-
-    }
-
-
-
-
-}
+        }}}
